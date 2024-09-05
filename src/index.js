0480756aaea4b3d17a990a5c7d221715082ab9e5
Ashish <ashishsinghnegi381@gmail.com>
2024-09-05 10:52:07 +0530

    apiresponce

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-/*import express from "express";
-const app = express();
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+connectDB()
+  .then(() => {
     app.on("error", () => {
-      console.log("Error:", error);
-      throw error;
+      console.log("Error on connection establishment", error);
     });
-    app.listen(process.env.PORT, () => {
-      console.log(`Process is listening on port ${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server started at port :", process.env.PORT);
     });
-  } catch (error) {
-    console.error("Error:", error);
-    throw err;
-  }
-})();
-*/
+  })
+  .catch((error) => {
+    console.error(`MongoDB connection failed!!! , ${error}`);
+  });
